83487bd6a51a63db0832782e4dbf06e07286c17f
Seijung Kim <sjtok@Seijungs-MacBook-Pro.local>
2022-09-05 12:53:37 -0400

    added check_HDL function that chracterizes HDL value

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,5 +12,13 @@ def user_input():
     user_HDL = input("Type your HDL result")
     return int(user_HDL) #since the input is string by default
 
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        return "Normal"
+    elif 40 <= HDL_value <60: #python can use this notation for compound inequality
+        return "Borderline Low"
+    else:
+        return "Low"
 
 interface()
+HDL_value = user_input()
