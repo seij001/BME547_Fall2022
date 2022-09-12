056fc411f5a672424e50afbfd03daa5984985873
Seijung Kim <sjtok@Seijungs-MacBook-Pro.local>
2022-09-12 12:45:56 -0400

    created formast_output function to print correct dosage in a separate module

--- dosing.py
+++ dosing.py
@@ -5,6 +5,11 @@
              type of medical treatment or diagnostics.
 """
 
+def format_output(weight, dosage_mg_first_day):
+    print("CORRECT DOSAGE")
+    print("For a patient weighing {:.1f} kg,".format(weight))
+    print("  the correct dosage is {:.1f} mg the first day"
+          .format(dosage_mg_first_day))
 
 def dose_amount():
     print("Day One Dosing Guidelines")
@@ -27,10 +32,7 @@ def dose_amount():
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
-    print("CORRECT DOSAGE")
-    print("For a patient weighing {:.1f} kg,".format(weight))
-    print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+    return weight, dosage_mg_first_day
 
 
 if __name__ == '__main__':
