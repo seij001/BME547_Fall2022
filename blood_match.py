c403c1259910453dc63ff43f92e5ded9e7c80fbe
Seijung Kim <sjtok@Seijungs-MacBook-Pro.local>
2022-10-23 17:21:00 -0400

    fixed pycodestyle syntax

--- blood_match.py
+++ blood_match.py
@@ -2,12 +2,6 @@ import requests
 
 
 # URL: http://vcm-7631.vm.duke.edu:5002
-'''Get the IDs for two patients by making a GET request to URL/get_patients/<name>.
-Replace <name> with your Duke Net ID.
-This request will return a dictionary in the following format:
-{"Recipient": "<ID1>", "Donor": "<ID2>"}.'''
-
-
 def get_ids():
     r = requests.get("http://vcm-7631.vm.duke.edu:5002/get_patients/sk591")
     print(r.status_code)
@@ -16,11 +10,6 @@ def get_ids():
     # {'Donor': 'M1', 'Recipient': 'F6'} in json
 
 
-
-'''
-Obtain the blood type of the two patients by making GET requests to URL/get_blood_type/<id> where <id> is replaced by either <ID1> 
-or <ID2> from above.
-This request will return a string with the blood type of the given patient.'''
 def get_bloodtype():
     r1 = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/M1")
     r2 = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/F6")
@@ -33,23 +22,11 @@ def get_bloodtype():
     # ('O+', 'AB-') in tuple
 
 
-# Calculate whether it is an acceptable match for the recipient to receive blood from the donor.
 # O+ cannot donate to AB-
-# O: donate RBC to all, receive from O
-# A: donate to A and AB, receive from A and O
-# B: donate to B and AB, receive from B and O
-# AB: donate to AB, receive from all
-
-'''
-Check your result by making a POST request to URL/match_check.
-Send a JSON with the following format:
-{"Name": "<name>", "Match": "<answer>"}
-Replace <name> with your Duke Net ID and <answer> with either Yes or No.
-This request will return "Correct" or "Incorrect".'''
-
 def check_match():
     out_data = {"Name": "sk591", "Match": "No"}
-    r = requests.post("http://vcm-7631.vm.duke.edu:5002/match_check", json=out_data)
+    r = requests.post("http://vcm-7631.vm.duke.edu:5002/match_check",
+                      json=out_data)
     print(r.status_code)
     print(r.text)
 
